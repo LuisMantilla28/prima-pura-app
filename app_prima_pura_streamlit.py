2df76885af9a4f1917ab13f25abfb782308e18d4
LuisMantilla28 <113846460+LuisMantilla28@users.noreply.github.com>
2025-10-16 14:06:48 -0500

    Update app_prima_pura_streamlit.py

--- app_prima_pura_streamlit.py
+++ app_prima_pura_streamlit.py
@@ -184,7 +184,7 @@ with col1:
     anio = st.selectbox("Año cursado", ["1ro", "2do", "3ro", "4to", "5to"], index=3)
     area = st.selectbox("Área de estudios", ["Ingenierías", "Humanidades", "Ciencias", "Economía", "Artes", "Salud"], index=1)
     genero = st.selectbox("Género", ["Masculino", "Femenino", "Otro"], index=0)
-    calif_prom = st.number_input("Calificación promedio", min_value=0.0, max_value=5.0, value=7.01, step=0.01)
+    calif_prom = st.number_input("Calificación promedio", min_value=0.0, max_value=10.0, value=7.01, step=0.01)
 with col2:
     dos_mas = st.selectbox("¿2 o más inquilinos?", ["No", "Sí"], index=0)
     en_campus = st.selectbox("¿Vive en campus?", ["No", "Sí"], index=0)
